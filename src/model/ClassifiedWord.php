8a024a94851f74843a10df41447f39b32619e38d
zioMitch <michele.carino84@gmail.com>
2017-11-19 10:41:41 +0100

    Added italian word model

--- src/model/ClassifiedWord.php
+++ src/model/ClassifiedWord.php
@@ -2,23 +2,36 @@
 
 namespace Pherserk\WordProvider\model;
 
+use Pherserk\Language\model\LanguageInterface;
 use Pherserk\Word\model\WordInterface;
 
 abstract class ClassifiedWord implements WordInterface
 {
    private $word;
    
+   private $language;
+
    private $type;
 
-   public function __construct(string $word, string $type)
+   public function __construct(string $word, LanguageInterface $language, string $type)
    {
+       if ($language->getCode() != $self::LANGUAGE_CODE) {
+           throw new \InvalidArgumentException('Expected '  . $self::LANGUAGE_CODE . ' '  $language->getCode() . ' given');
+       }
+       
        $this->word = $word;
+       $this->language = $language;
        $this->type = $type;
    }
 
    public function getWord() : string
    {
        return $this->word;
+   }
+
+   public function getLanguage() : LanguageInterface
+   {
+       return $this->language;
    }
   
    public function getType() : string
---
+++ src/model/it/ClassifiedWord.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Pherserk\WordProvider\model\it;
+
+use Pherserk\WordProvider\model\ClassifiedWord as BaseClassifiedWord;
+
+class ClassifiedWord extends BaseClassifiedWord
+{
+    const LANGUAGE_CODE = 'IT';
+
+    const NAME_TYPE = 'name';
+    const VERB_TYPE = 'verb';
+    const PRONOUN_TYPE = 'pronoun';
+    const ARTICLE_TYPE = 'article';
+    const PREPOSITION_TYPE = 'preposition';
+    const ADVERB_TYPE = 'adverb';
+    const CONJUNCTION = 'conjunction';    
+
+    private $word;
+ 
+    private $language;
+ 
+    private $type;
+
+    public function __construct(string $word, LanguageInterface $language, string $type)
+    {
+        parent::__construct($word, $language, $type);
+    } 
+}
